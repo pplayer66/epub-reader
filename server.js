b09c4a3d890271813f7ffb5fe552414d9d125c15
Yerden Abdygapparov <erden.a@bilimmail.kz>
2017-09-20 14:06:17 +0600

    edit routing

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 2000;
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'pug');
 app.use(express.static('reader'));
 
-app.get('/reader', (req, res)=>{
+app.get('/', (req, res)=>{
 	res.render('index');
+});
+
+app.get('*', (req, res)=>{
+	res.redirect('/');
 });
 
 app.listen(port, ()=>console.log('server is running on port', port));
