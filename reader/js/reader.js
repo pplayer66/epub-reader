b4415aec414331901d5131e07a4e8c044bbf69ad
pplayer66 <lander89@inbox.ru>
2017-09-21 15:12:05 +0600

    .

--- reader/js/reader.js
+++ reader/js/reader.js
@@ -19,6 +19,35 @@ EPUBJS.reader.plugins = {}; //-- Attach extra Controllers as plugins (like searc
 
 })(window, jQuery);
 
+EPUBJS.Hooks.register("beforeChapterDisplay").pageTurns = function (callback, renderer) {
+    var lock = false;
+    var arrowKeys = function (e) {
+        e.preventDefault();
+        if (lock) return;
+
+        if (e.keyCode == 37) {
+            ePubViewer.Book.prevPage();
+            lock = true;
+            setTimeout(function () {
+                lock = false;
+            }, 100);
+            return false;
+        }
+
+        if (e.keyCode == 39) {
+            ePubViewer.Book.nextPage();
+            lock = true;
+            setTimeout(function () {
+                lock = false;
+            }, 100);
+            return false;
+        }
+
+    };
+    renderer.doc.addEventListener('keydown', arrowKeys, false);
+    if (callback) callback();
+}
+
 EPUBJS.Reader = function(bookPath, _options) {
 		var reader = this;
 		var book;
