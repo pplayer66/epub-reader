c3a1787887483b416b5f751e81f0a6d5ce0bd48a
pplayer66 <lander89@inbox.ru>
2017-09-27 14:08:52 +0600

    .

--- reader/js/init.js
+++ reader/js/init.js
@@ -125,9 +125,9 @@ document.onreadystatechange = function () {
 				})
 		};
 
-		// book.on('renderer:visibleRangeChanged', sendDataCfi);
+		book.on('renderer:visibleRangeChanged', sendDataCfi);
 		// addWindowResizeListener();
 		// fetchDataCfi();
-		addBook();
+		// addBook();
 	}
 };
