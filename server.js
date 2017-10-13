d346ed97a767ccfcb6bbaa8e8579e8327971c381
Yerden Abdygapparov <erden.a@bilimmail.kz>
2017-10-13 14:43:56 +0600

    .

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
-// const port = process.env.PORT || 3000;
-const port = 3000;
+const port = process.env.PORT || 3000;
+// const port = 3000;
 const book = require('./routes/books');
 
 const {Book} = require('./models/Book');
